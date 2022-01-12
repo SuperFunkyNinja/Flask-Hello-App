c6803b8e629a68a25a361397403b8ca0d29f4fd1
SuperFunkyNinja <superfunkyninja@gmail.com>
2022-01-12 19:13:06 +0000

    updated

--- main2.py
+++ main2.py
@@ -10,19 +10,19 @@ def index():
     pan_value = request.args.get("pan_value", "")
     tilt_value = request.args.get("tilt_value", "")
     if pan_value:
-        tilt_value = tilt_value_from(pan_value)
+        pan_value = tilt_value_from(pan_value)
     if tilt_value:
-        pan_value = pan_value_from(tilt_value)
+        tilt_value = pan_value_from(tilt_value)
     return (
         """<form action="" method="get">
-                pan_value: <input type="text" name="pan_value">
-                tilt_value: <input type="text" name="tilt_value">
+                Pan Value: <input type="text" name="pan_value">
+                Tilt Value: <input type="text" name="tilt_value">
                 <input type="submit" value="Activate">
             </form>"""
-        + "tilt_value: "
+        + "Tilt Value: "
         + tilt_value
         + " "
-        + "pan_value: "
+        + "Pan Value: "
         + pan_value
     )
 
@@ -38,7 +38,7 @@ def tilt_value_from(pan_value):
 def pan_value_from(tilt_value):
     """Convert tilt_value to Celius degrees."""
     try:
-        pantilthat.tilt(int(tilt_value))
+        pantilthat.tilt(-int(tilt_value))
         return str(tilt_value)
     except ValueError:
         return "invalid input"
